7f53bb2b55200b363e9d61b85f0d12fd2df4ade0
Juan Beltrame <61248615+JuanBeltrame@users.noreply.github.com>
2023-10-05 09:43:17 -0300

    Update (Beecrowd) Beginner - 1001 - Extremely Basic.py

--- (Beecrowd) Beginner - 1001 - Extremely Basic.py
+++ (Beecrowd) Beginner - 1001 - Extremely Basic.py
@@ -1,4 +1,6 @@
 """
+Link to the problem: https://www.beecrowd.com.br/judge/en/problems/view/1001
+----------------------------------------------------------------------
 Read 2 variables, 
 named A and B and make the sum of these two variables, 
 assigning its result to the variable X. 
