b355e86dc0bf8b2a0656c75f2572d296ae68b4d5
Juan Beltrame <61248615+JuanBeltrame@users.noreply.github.com>
2023-10-05 09:42:28 -0300

    Update (Beecrowd) Beginner - 1000 - Hello World!.py

--- (Beecrowd) Beginner - 1000 - Hello World!.py
+++ (Beecrowd) Beginner - 1000 - Hello World!.py
@@ -1,4 +1,6 @@
 """
+Link to the problem: https://www.beecrowd.com.br/judge/en/problems/view/1000
+----------------------------------------------------------------------
 Your first program in any programming language is usually "Hello World!". 
 In this first problem all you have to do is print this message on the screen.
 
